549a1b9c052e29f26992be937166b349e23c5eef
HangyAnt <gabor.hangyasi@gmail.com>
2021-01-04 13:53:41 +0100

    getters and setters added

--- src/main/java/com/codecool/oopws1/Main.java
+++ src/main/java/com/codecool/oopws1/Main.java
@@ -3,9 +3,16 @@ package com.codecool.oopws1;
 public class Main {
 
     public static void main(String[] args) {
-        Range range = new Range();
-        range.setStart(10);
-        range.setEnd(100);
+        Range range1 = new Range(5, 10);
+        range1.setStart(15);
+        range1.setEnd(1);
+        System.out.println("range1 start: " + range1.getStart());
+        System.out.println("range1 end: " + range1.getEnd());
+        Range range2 = new Range();
+        range2.setEnd(90);
+        range2.setStart(10);
+        System.out.println("range2 start: " + range2.getStart());
+        System.out.println("range2 end: " + range2.getEnd());
     }
 
 }
--- src/main/java/com/codecool/oopws1/Range.java
+++ src/main/java/com/codecool/oopws1/Range.java
@@ -6,12 +6,39 @@ public class Range {
 
     private int end;
 
+    Range() {
+    }
+
+    Range(int start, int end) {
+        if (end > start) {
+            this.start = start;
+            this.end = end;
+        } else {
+            System.out.println("Invalid values.");
+        }
+    }
+
     void setStart(int start) {
-        this.start = start;
+        if (this.end > start) {
+            this.start = start;
+        } else {
+            System.out.println("\"Start\" must be smaller than \"end\". Try to set \"end\" first.");
+        }
     }
 
     void setEnd(int end) {
-        this.end = end;
+        if (end > this.start) {
+            this.end = end;
+        } else {
+            System.out.println("\"End\" must be bigger than \"start\".");
+        }
     }
-    
+
+    int getStart() {
+        return this.start;
+    }
+
+    int getEnd() {
+        return this.end;
+    }
 }
