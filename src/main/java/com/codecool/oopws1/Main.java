299168a61424dfd8186a0ba43f60765221d7b053
HangyAnt <gabor.hangyasi@gmail.com>
2021-01-04 13:29:27 +0100

    src folder, Main and Range classes added

---
+++ src/main/java/com/codecool/oopws1/Main.java
@@ -0,0 +1,11 @@
+package com.codecool.oopws1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Range range = new Range();
+        range.setStart(10);
+        range.setEnd(100);
+    }
+
+}
---
+++ src/main/java/com/codecool/oopws1/Range.java
@@ -0,0 +1,17 @@
+package com.codecool.oopws1;
+
+public class Range {
+
+    private int start;
+
+    private int end;
+
+    void setStart(int start) {
+        this.start = start;
+    }
+
+    void setEnd(int end) {
+        this.end = end;
+    }
+    
+}
